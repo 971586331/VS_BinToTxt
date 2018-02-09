d73684bde522901fcbab7d48951cb7dafedda520
shiyu <971586331@qq.com>
2018-02-09 17:56:24 +0800

    添加了队列，正在处理遍历文件夹
    如何遍历一个目录，健康找出目录下的所有文件和文件夹

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,211 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox_open = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_open = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_save = new System.Windows.Forms.TextBox();
+            this.button_save = new System.Windows.Forms.Button();
+            this.button_check = new System.Windows.Forms.Button();
+            this.label_filenume = new System.Windows.Forms.Label();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label_CurrentInput = new System.Windows.Forms.Label();
+            this.label_CurrentOutput = new System.Windows.Forms.Label();
+            this.label_ShowTime = new System.Windows.Forms.Label();
+            this.label_RunTime = new System.Windows.Forms.Label();
+            this.label_SystemResource = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_open
+            // 
+            this.textBox_open.Location = new System.Drawing.Point(10, 35);
+            this.textBox_open.Name = "textBox_open";
+            this.textBox_open.Size = new System.Drawing.Size(280, 21);
+            this.textBox_open.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "打开要转换的bin文件目录";
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(300, 32);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(75, 23);
+            this.button_open.TabIndex = 2;
+            this.button_open.Text = "打开";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "保存转换后的txt文件";
+            // 
+            // textBox_save
+            // 
+            this.textBox_save.Location = new System.Drawing.Point(10, 90);
+            this.textBox_save.Name = "textBox_save";
+            this.textBox_save.Size = new System.Drawing.Size(280, 21);
+            this.textBox_save.TabIndex = 4;
+            // 
+            // button_save
+            // 
+            this.button_save.Location = new System.Drawing.Point(300, 90);
+            this.button_save.Name = "button_save";
+            this.button_save.Size = new System.Drawing.Size(75, 23);
+            this.button_save.TabIndex = 5;
+            this.button_save.Text = "保存";
+            this.button_save.UseVisualStyleBackColor = true;
+            this.button_save.Click += new System.EventHandler(this.button_save_Click);
+            // 
+            // button_check
+            // 
+            this.button_check.Location = new System.Drawing.Point(23, 136);
+            this.button_check.Name = "button_check";
+            this.button_check.Size = new System.Drawing.Size(100, 23);
+            this.button_check.TabIndex = 6;
+            this.button_check.Text = "检查输入参数";
+            this.button_check.UseVisualStyleBackColor = true;
+            this.button_check.Click += new System.EventHandler(this.button_check_Click);
+            // 
+            // label_filenume
+            // 
+            this.label_filenume.AutoSize = true;
+            this.label_filenume.Location = new System.Drawing.Point(23, 176);
+            this.label_filenume.Name = "label_filenume";
+            this.label_filenume.Size = new System.Drawing.Size(65, 12);
+            this.label_filenume.TabIndex = 7;
+            this.label_filenume.Text = "文件个数：";
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(272, 136);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(100, 23);
+            this.button_start.TabIndex = 8;
+            this.button_start.Text = "开始转换";
+            this.button_start.UseVisualStyleBackColor = true;
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(272, 165);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(100, 23);
+            this.button_stop.TabIndex = 9;
+            this.button_stop.Text = "结果转换";
+            this.button_stop.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(23, 207);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(347, 23);
+            this.progressBar1.TabIndex = 10;
+            // 
+            // label_CurrentInput
+            // 
+            this.label_CurrentInput.AutoSize = true;
+            this.label_CurrentInput.Location = new System.Drawing.Point(21, 242);
+            this.label_CurrentInput.Name = "label_CurrentInput";
+            this.label_CurrentInput.Size = new System.Drawing.Size(65, 12);
+            this.label_CurrentInput.TabIndex = 11;
+            this.label_CurrentInput.Text = "正在转换：";
+            // 
+            // label_CurrentOutput
+            // 
+            this.label_CurrentOutput.AutoSize = true;
+            this.label_CurrentOutput.Location = new System.Drawing.Point(21, 264);
+            this.label_CurrentOutput.Name = "label_CurrentOutput";
+            this.label_CurrentOutput.Size = new System.Drawing.Size(65, 12);
+            this.label_CurrentOutput.TabIndex = 12;
+            this.label_CurrentOutput.Text = "输出文件：";
+            // 
+            // label_ShowTime
+            // 
+            this.label_ShowTime.AutoSize = true;
+            this.label_ShowTime.Location = new System.Drawing.Point(21, 291);
+            this.label_ShowTime.Name = "label_ShowTime";
+            this.label_ShowTime.Size = new System.Drawing.Size(65, 12);
+            this.label_ShowTime.TabIndex = 13;
+            this.label_ShowTime.Text = "当前时间：";
+            // 
+            // label_RunTime
+            // 
+            this.label_RunTime.AutoSize = true;
+            this.label_RunTime.Location = new System.Drawing.Point(21, 313);
+            this.label_RunTime.Name = "label_RunTime";
+            this.label_RunTime.Size = new System.Drawing.Size(65, 12);
+            this.label_RunTime.TabIndex = 14;
+            this.label_RunTime.Text = "运行时间：";
+            // 
+            // label_SystemResource
+            // 
+            this.label_SystemResource.AutoSize = true;
+            this.label_SystemResource.Location = new System.Drawing.Point(21, 335);
+            this.label_SystemResource.Name = "label_SystemResource";
+            this.label_SystemResource.Size = new System.Drawing.Size(65, 12);
+            this.label_SystemResource.TabIndex = 15;
+            this.label_SystemResource.Text = "系统资源：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.label_SystemResource);
+            this.Controls.Add(this.label_RunTime);
+            this.Controls.Add(this.label_ShowTime);
+            this.Controls.Add(this.label_CurrentOutput);
+            this.Controls.Add(this.label_CurrentInput);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.label_filenume);
+            this.Controls.Add(this.button_check);
+            this.Controls.Add(this.button_save);
+            this.Controls.Add(this.textBox_save);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_open);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox_open;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_save;
+        private System.Windows.Forms.Button button_save;
+        private System.Windows.Forms.Button button_check;
+        private System.Windows.Forms.Label label_filenume;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label_CurrentInput;
+        private System.Windows.Forms.Label label_CurrentOutput;
+        private System.Windows.Forms.Label label_ShowTime;
+        private System.Windows.Forms.Label label_RunTime;
+        private System.Windows.Forms.Label label_SystemResource;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿ 
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,14 +7,75 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace VS_BinToTxt
 {
     public partial class Form1 : Form
     {
+
+        struct Data_Node
+        {
+            string InputFilePath;    //输入文件的路径
+            string OutputFilePath;   //输出文件的路径
+        };
+
+        Queue<Data_Node> gQueue_Data = new Queue<Data_Node>();
+
+        void traverse_dir(string path, ref Queue<Data_Node> queue)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+            FileInfo[] fil = dir.GetFiles();
+            DirectoryInfo[] dii = dir.GetDirectories();
+
+            //获取子文件夹内的文件列表，递归遍历  
+            foreach (DirectoryInfo d in dii)
+            {
+                //traverse_dir(d.Name, ref queue);
+                Console.WriteLine("dir = {0}", d.Name);
+            } 
+
+            foreach (FileInfo f in fil)
+            {
+                Data_Node temp;
+
+                Console.WriteLine("file = {0}\\{1}", f.DirectoryName, f.Name);
+                //queue.Enqueue(temp);
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择bin文件目录";
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                string sPath = folder.SelectedPath;
+                this.textBox_open.Text = sPath;
+            }
+        }
+
+        private void button_save_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择txt存放目录";
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                string sPath = folder.SelectedPath;
+                this.textBox_save.Text = sPath;
+            }
+        }
+
+        private void button_check_Click(object sender, EventArgs e)
+        {
+            string path = this.textBox_open.Text;
+            traverse_dir(path, ref gQueue_Data);
+        }
+
     }
 }
